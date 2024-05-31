516874c609582ca845de2e13577377a7a1059514
Leta Asefa <letaassefa7@gmail.com>
2024-05-31 10:34:10 +0300

    Delete Files And Their References Too !

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Partials/NavBar';
-import Footer from './Components/Partials/Footer';
-import SideBar from './Components/Partials/SideBar';
-import MenuBar from './Components/Partials/MenuBar';
 import Signup from './Components/Auth/SignUp';
 import Login from './Components/Auth/Login';
-import FolderList from './Components/FolderList';
 import Home from './Components/Home';
-import PathBar from './Components/Partials/PathBar';
-import CreateNewFolder from './Components/PopUps/CreateNewFolder';
 import AddUsers from './Components/AddUsers';
 import UsersControl from './Components/UsersControl';
 import ChangePassword from './Components/ChangePassword';
+import AuthLayout from './Components/Layouts/AuthLayout';
+import MainLayout from './Components/Layouts/MainLayout';
 
 function App() {
   return (
-
-
-
     <Router>
-      <Navbar />
+      <Routes>
+        {/* Routes that use MainLayout */}
+        <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+          <Route path="/home/:id?" element={<Home />} />
+          <Route path="/adduser" element={<AddUsers />} />
+          <Route path="/alloweduser" element={<UsersControl />} />
+          <Route path="/changepassword" element={<ChangePassword />} />
+        </Route>
 
-      <div className='flex'>
-        <div className='flex-none'><SideBar /></div>
-        <div className='flex-grow bg-slate-200'>
-          <MenuBar />
-          <PathBar/>
-          <div>
-          <Routes>
-              <Route path="/home/:id?" element={<Home />} />
-              <Route path="/adduser" element={<AddUsers />} />
-              <Route path="/alloweduser" element={<UsersControl />} />
-              <Route path="/changepassword" element={<ChangePassword />} />
-           </Routes>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
+        {/* Routes that use AuthLayout */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+      </Routes>
     </Router>
   );
 }
